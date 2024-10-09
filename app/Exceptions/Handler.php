d7654df4be7cc04a411093b2714e374f1edc4ca2
fatih koc <fathkccc@gmail.com>
2024-10-09 22:21:02 +0300

    rate limit && handler && test

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * Report or log an exception.
+     */
+    public function report(Throwable $exception): void
+    {
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     */
+    public function render($request, Throwable $exception)
+    {
+        // Rate limiting sınırına ulaşıldığında özelleştirilmiş mesaj
+        if ($exception instanceof TooManyRequestsHttpException) {
+            return response()->json([
+                'message' => 'Çok fazla istek yaptınız. Lütfen 1 dakika sonra tekrar deneyin.'
+            ], 429);
+        }
+
+        return parent::render($request, $exception);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
 
-Route::get('/blogs', [BlogController::class, 'index'])->name('blogs.index');
-Route::get('/blogs/create', [BlogController::class, 'create'])->name('blogs.create');
-Route::post('/blogs', [BlogController::class, 'store'])->name('blogs.store');
-Route::get('/blogs/{id}', [BlogController::class, 'show'])->name('blogs.show');
-Route::get('/blogs/{id}/edit', [BlogController::class, 'edit'])->name('blogs.edit');
-Route::put('/blogs/{id}', [BlogController::class, 'update'])->name('blogs.update');
-Route::delete('/blogs/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy');
+
+Route::middleware('throttle:60,1')->group(function () {
+    Route::get('/blogs', [BlogController::class, 'index'])->name('blogs.index');
+    Route::get('/blogs/create', [BlogController::class, 'create'])->name('blogs.create');
+    Route::post('/blogs', [BlogController::class, 'store'])->name('blogs.store');
+    Route::get('/blogs/{id}', [BlogController::class, 'show'])->name('blogs.show');
+    Route::get('/blogs/{id}/edit', [BlogController::class, 'edit'])->name('blogs.edit');
+    Route::put('/blogs/{id}', [BlogController::class, 'update'])->name('blogs.update');
+    Route::delete('/blogs/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy');
+});
